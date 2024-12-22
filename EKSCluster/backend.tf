b18268e556665ebff2193b941c3ac1561e49348c
adnanahmad85 <164812352+adnanahmad85@users.noreply.github.com>
2024-12-22 13:49:51 +0000

    Update backend.tf

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" #kubernetesproject1
+    bucket = "XX" # "kubernetesproject1"
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
